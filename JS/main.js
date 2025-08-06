9252d35181e04feb8409f192a0e2f0997ef93b97
sebastian david hernandez <96744129+SebastianHernandezDev@users.noreply.github.com>
2025-08-06 17:27:11 -0500

    final

--- JS/main.js
+++ JS/main.js
@@ -151,10 +151,3 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 });
-const cards = document.querySelectorAll('.card-surf');
-
-cards.forEach(card => {
-  card.addEventListener('click', () => {
-    card.classList.toggle('flipped'); 
-  });
-});
